85a3847d0e6fdfd7840927d8d37cb1d6c051a1c6
Enrico Maria De Angelis <enricomaria.dean6elis@gmail.com>
2023-10-14 10:07:04 +0100

    wip

--- main.js
+++ main.js
@@ -7,18 +7,26 @@ window.onkeyup = listenKeyUp;
 // lascia Alt colorato. Succede non solo per Alt, ma anche per
 // Home/End e le 4 frecce. Non succede per Ctrl.
 //
+// TODO:
+// Per ottenere il tasto relativo a un codice:
+// await navigator.keyboard.getLayoutMap().then(klm => klm.get(e.code))
 function listenKeyDown(e) {
   console.log(e)
   if (e.altKey) {
     document.getElementById('Alt').style.background = '#00ff00';
   }
   if (e.ctrlKey) {
-    document.getElementById('CtrlLeft').style.background = '#00ff00';
+    document.getElementById(e.code).style.background = '#00ff00';
   }
   if (e.shiftKey) {
     document.getElementById('ShiftLeft').style.background = '#00ff00';
   }
-  let key = e.key != ' ' ? e.key : 'Space';
+  let key = e.key;
+  if (key >= '0' && key <= '9') {
+    key = e.code;
+  }
+  if (key == ' ')
+    key = 'Space';
   let elem = document.getElementById(key);
   if (elem && !e.altKey && !e.ctrlKey && !e.shiftKey) {
     elem.style.background = '#00ff00';
@@ -31,12 +39,17 @@ function listenKeyUp(e) {
     document.getElementById('Alt').style.background = 'none';
   }
   if (e.ctrlKey) {
-    document.getElementById('CtrlLeft').style.background = 'none';
+    document.getElementById(e.code).style.background = 'none';
   }
   if (e.shiftKey) {
     document.getElementById('ShiftLeft').style.background = 'none';
   }
-  let key = e.key != ' ' ? e.key : 'Space';
+  let key = e.key;
+  if (key >= '0' && key <= '9') {
+    key = e.code;
+  }
+  if (key == ' ')
+    key = 'Space';
   let elem = document.getElementById(key);
   if (elem && !e.altKey && !e.ctrlKey && !e.shiftKey) {
     document.getElementById(key).style.background = 'none';
