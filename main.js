217eb2dd6ed54fc9ff08dcd45ad19bfa747476d8
Enrico Maria De Angelis <enricomaria.dean6elis@gmail.com>
2023-10-15 09:06:27 +0100

    wip

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+window.onload =  hit_key;
 window.onkeydown = listenKeyDown;
 window.onkeyup = listenKeyUp;
 
@@ -7,13 +8,15 @@ window.onkeyup = listenKeyUp;
 // lascia Alt colorato. Succede non solo per Alt, ma anche per
 // Home/End e le 4 frecce. Non succede per Ctrl.
 
-async function getKey(e) {
-  let maybe_key = await navigator.keyboard.getLayoutMap().then(klm => klm.get(e.code));
-  return maybe_key ? maybe_key : e.key;
+function hit_key() {
+  window.dispatchEvent(new KeyboardEvent('keydown', {'key': 'Escape'}));
+  window.dispatchEvent(new KeyboardEvent('keyup', {'key': 'Escape'}));
 }
 
 async function listenKeyDown(e) {
   console.log(e)
+  document.getElementById('CapsLock').style.background =
+    e.getModifierState('CapsLock') ? '#00ff00' : 'none';
   let elem = await getElementByEvent(e);
   elem.style.background = '#00ff00';
 }
@@ -39,4 +42,9 @@ async function getElementByEvent(e) {
     key = e.code;
   }
   return document.getElementById(key);
+}
+
+async function getKey(e) {
+  let maybe_key = await navigator.keyboard.getLayoutMap().then(klm => klm.get(e.code));
+  return maybe_key ? maybe_key : e.key;
 }
