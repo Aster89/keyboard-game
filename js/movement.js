f2cf60d182bddb793f77da99449fe7074886d6c9
Enrico Maria De Angelis <enricomaria.dean6elis@gmail.com>
2023-10-16 23:50:54 +0100

    wip

--- js/movement.js
+++ js/movement.js
@@ -21,14 +21,22 @@ function movecursor(e) {
   } else if (['ArrowUp', 'ArrowDown'].includes(e.key)) {
     let cur = par.children['cursor'];
     let curpos = cur.getAttribute('pos');
-    if ('ArrowUp') {
+    let higher_lefter = (elem, cur) => elem.offsetTop < cur.offsetTop && elem.offsetLeft < cur.offsetLeft;
+    let lower_rigther = (elem, cur) => elem.offsetTop > cur.offsetTop && elem.offsetLeft > cur.offsetLeft;
+    let isspan = elem => elem.tagName == 'SPAN';
+    if ('ArrowUp' == e.key) {
       let offset = Array.from(par.children)
         .slice(0, curpos)
         .reverse()
-        .findIndex(elem => elem.tagName == 'SPAN' && elem.offsetLeft < cur.offsetLeft && elem.offsetTop < cur.offsetTop);
-      let displs = [-1, 0].map(i => Math.abs(par.children[cur.getAttribute('pos') - offset + i].offsetLeft - cur.offsetLeft));
-      advance(offset + [1, 0][displs.indexOf(Math.min(...displs))], -1);
+        .findIndex(elem => isspan(elem) && higher_lefter(elem, cur));
+      let displs = [-1, 0].map(i => Math.abs(par.children[Number(cur.getAttribute('pos')) - offset + i].offsetLeft - cur.offsetLeft));
+      advance(offset - [-1, 0][displs.indexOf(Math.min(...displs))], -1);
     } else {
+      let offset = Array.from(par.children)
+        .slice(curpos)
+        .findIndex(elem => isspan(elem) && lower_rigther(elem, cur));
+      let displs = [-1, 0].map(i => Math.abs(par.children[Number(cur.getAttribute('pos')) + offset + i].offsetLeft - cur.offsetLeft));
+      advance(offset + [-1, 0][displs.indexOf(Math.min(...displs))] - 1, 1);
     }
   }
 }
