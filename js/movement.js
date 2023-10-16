0926d0444f466e0119b2e4149e6e5aeb25a3e3dc
Enrico Maria De Angelis <enricomaria.dean6elis@gmail.com>
2023-10-16 22:58:21 +0100

    wip

--- js/movement.js
+++ js/movement.js
@@ -7,7 +7,7 @@ function advance(steps, dir) {
   cursor.setAttribute('pos', curpos + offset);
 }
 
-function eow(dir) {
+function stride(dir) {
   let curpos = Number(cursor.getAttribute('pos'));
   return dir == 1
     ? RegExp(/^ *([a-zA-Z0-9]+|.|\n)/).exec(text.substr(curpos))[0].length
@@ -17,6 +17,18 @@ function eow(dir) {
 function movecursor(e) {
   if (['ArrowLeft', 'ArrowRight'].includes(e.key)) {
     let dir = e.key == 'ArrowLeft' ? -1 : 1;
-    advance(e.ctrlKey ? eow(dir) : 1, dir);
+    advance(e.ctrlKey ? stride(dir) : 1, dir);
+  } else if (['ArrowUp', 'ArrowDown'].includes(e.key)) {
+    let cur = par.children['cursor'];
+    let curpos = cur.getAttribute('pos');
+    if ('ArrowUp') {
+      let offset = Array.from(par.children)
+        .slice(0, curpos)
+        .reverse()
+        .findIndex(elem => elem.tagName == 'SPAN' && elem.offsetLeft < cur.offsetLeft && elem.offsetTop < cur.offsetTop);
+      let displs = [-1, 0].map(i => Math.abs(par.children[cur.getAttribute('pos') - offset + i].offsetLeft - cur.offsetLeft));
+      advance(offset + [1, 0][displs.indexOf(Math.min(...displs))], -1);
+    } else {
+    }
   }
 }
