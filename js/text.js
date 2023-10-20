29c4ccf84e856ddb5397fd20274d50305b8299b3
Enrico Maria De Angelis <enricomaria.dean6elis@gmail.com>
2023-10-20 17:02:59 +0100

    wip

--- js/text.js
+++ js/text.js
@@ -2,9 +2,17 @@ let text = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi conva
 Cras urna velit, venenatis non urna imperdiet, dignissim iaculis nunc. Etiam accumsan, ligula vitae malesuada posuere, metus elit vestibulum mauris, pellentesque gravida lectus nisi vel elit. Fusce turpis eros, vehicula et sapien vitae, fringilla venenatis quam. Aliquam auctor posuere dui a eleifend. Duis velit nisl, laoreet sit amet quam at, pharetra feugiat neque. Phasellus imperdiet sodales ligula vitae tincidunt. Curabitur a maximus dui, id imperdiet enim. Vivamus vitae molestie leo.
 In bibendum, sem nec lacinia ornare, ex elit aliquam felis, accumsan aliquet metus orci quis ex. In hac habitasse platea dictumst. Cras imperdiet tortor ac eros posuere vestibulum. Proin vitae malesuada neque. Etiam facilisis euismod nisi. Morbi id sem vestibulum, malesuada diam ac, rutrum velit. Nunc non felis sit amet mauris volutpat rhoncus non a odio. Integer nulla dolor, fermentum sed mi in, pharetra convallis sem. Vivamus maximus nisl nec elit bibendum scelerisque. Fusce quam nisi, vulputate ac mattis accumsan, congue et nisi. Sed pretium gravida ligula efficitur cursus. Donec faucibus molestie mi, sit amet luctus metus fringilla in. Nam eleifend ut diam ac sollicitudin. Integer sit amet risus id risus dictum tincidunt id vitae tellus. Nulla ac imperdiet est. Proin mauris lacus, viverra et purus id, dignissim vehicula est.`;
 
-let makeSpan = text => {
+let makeSpan = () => document.createElement('span');
+
+let makeSpanOfText = text => {
   let s = document.createElement('span');
   s.appendChild(document.createTextNode(text));
+  return s;
+};
+
+let makeSpanWithClass = cls => {
+  let s = document.createElement('span');
+  s.classList.add(cls);
   return s;
 };
 
@@ -16,7 +24,7 @@ let par = (() => {
 
   text
     .split('\n')
-    .flatMap(line => line.split('').map(makeSpan).concat(makebreak()))
+    .flatMap(line => line.split('').map(makeSpanOfText).concat(makebreak()))
     .forEach(elem => { p.appendChild(elem); });
 
   let keyboard = document.getElementsByTagName('body')[0].children[0];
@@ -29,21 +37,22 @@ console.assert(text.length + 1/* trailing line break */ == par.children.length);
 
 let chars = document.getElementById('tokenized-text').children.length;
 
-let cursor = document.createElement('span');
+[Math.floor(Math.random() * chars), Math.floor(Math.random() * chars)]
+  .sort()
+  .map(pos => par.insertBefore(makeSpanWithClass('highlighted'), par.children[pos]));
+
+let cursor = makeSpan();
 cursor.id = 'cursor';
 cursor.classList.add('blinking');
 
 let rndpos = Math.floor(Math.random() * chars);
 
 cursor.setAttribute('pos', rndpos);
+par.insertBefore(cursor, par.children[rndpos]);
 
-let rndspan = par.children[rndpos];
-
-par.insertBefore(cursor, rndspan);
-
-let pos = cursor.getAttribute('pos');
-console.log('cursor is between "' + text.substr(pos-6, 6)
-                       + '" and "' + text.substr(pos, 6) + '"');
+//let pos = cursor.getAttribute('pos');
+//console.log('cursor is between "' + text.substr(pos-6, 6)
+//                       + '" and "' + text.substr(pos, 6) + '"');
 
 // TODO:
 // Per la selezione del testo una strategia sembra essere quella
