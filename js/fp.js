3e27b631263b5002d94318a6ca9d84329393d77d
Enrico Maria De Angelis <enricomaria.dean6elis@gmail.com>
2023-10-28 15:32:59 +0100

    simplify groupBy

--- js/fp.js
+++ js/fp.js
@@ -1,9 +1,8 @@
 groupBy = b => xs => {
-    if (xs == []) return [];
-    res = [[xs[0]]];
+    res = [xs.slice(0, 1)];
     xs.splice(1).forEach(x => {
         if (res.at(-1) == [] || b(res.at(-1).at(-1), x))
-            res[res.length -1] = res.at(-1).concat(x);
+            res[res.length - 1] = res.at(-1).concat(x);
         else
             res = res.concat([[x]]);
     });
