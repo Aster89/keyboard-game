93418c6f44e3ceee417fc252a410bdd13de8f58b
Enrico Maria De Angelis <enricomaria.dean6elis@gmail.com>
2023-10-29 12:02:23 +0000

    Rename a class considering I will have a target cursor too

--- js/target.js
+++ js/target.js
@@ -1,3 +1,3 @@
 [Math.floor(Math.random() * text.length()), Math.floor(Math.random() * text.length())]
   .sort((a, b) => a - b)
-  .map((pos, i) => text.addClassAt(['target-beg', 'target-end'][i], pos));
+  .map((pos, i) => text.addClassAt(['target-selection-beg', 'target-selection-end'][i], pos));
